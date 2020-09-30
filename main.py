9854a4979c95a944d1df771747394cc1b624e2f8
yaiir-a <42359972+yaiir-a@users.noreply.github.com>
2020-09-30 21:53:58 +0100

    fix to handle changes to CORS policy
    As per the top comment on the youtube video, running the code without this argument just throws up these errors in the console:
    
    Access to XMLHttpRequest ..... has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @socketio.on('message')
 def handleMessage(msg):
